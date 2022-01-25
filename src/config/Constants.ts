06b79600a324b160d323638ebd21176874143e4f
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-01-25 10:07:37 -0400

    add conexão mongoDB

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,5 @@
 export const config = {
-  API_URL: "https://github.com/Wanderson-A-Timoteo",
+  API_URL: "http://localhost:5000",
+  MONGO_CONNECTION:
+    "mongodb+srv://wanderson:<12345>@url-shortener.svhwp.mongodb.net/url-shortener?retryWrites=true&w=majority",
 };
